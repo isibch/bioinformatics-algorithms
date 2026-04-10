6f973cf01d794af674644fcb858c6f2edaf1e2ba
Isabelle Bach <bachis@cip.ifi.lmu.de>
2026-04-10 17:36:58 +0200

    updating init + api

--- src/bioalgos/alignment/api.py
+++ src/bioalgos/alignment/api.py
@@ -1,8 +1,21 @@
 from .needleman_wunsch import needleman_wunsch
 from .smith_waterman import smith_waterman
+from .gotoh_global import gotoh_global
+from .gotoh_local import gotoh_local
+from .gotoh_freeshift import gotoh_freeshift
 
-def align(seq1, seq2, method="nw", match_score=1, mismatch_score=-1, gap_cost=1):
-    if method in {"needleman-wunsch", "nw"}:
+
+def align(
+    seq1,
+    seq2,
+    method="nw",
+    match_score=1,
+    mismatch_score=-1,
+    gap_cost=1,
+    gap_open=-5,
+    gap_extend=-1,
+):
+    if method in {"nw", "needleman-wunsch"}:
         return needleman_wunsch(
             seq1,
             seq2,
@@ -11,7 +24,7 @@ def align(seq1, seq2, method="nw", match_score=1, mismatch_score=-1, gap_cost=1)
             gap_cost=gap_cost,
         )
 
-    if method in {"smith-waterman", "sw"}:
+    if method in {"sw", "smith-waterman"}:
         return smith_waterman(
             seq1,
             seq2,
@@ -20,6 +33,36 @@ def align(seq1, seq2, method="nw", match_score=1, mismatch_score=-1, gap_cost=1)
             gap_cost=gap_cost,
         )
 
+    if method in {"gotoh-global", "gg"}:
+        return gotoh_global(
+            seq1,
+            seq2,
+            match_score=match_score,
+            mismatch_score=mismatch_score,
+            gap_open=gap_open,
+            gap_extend=gap_extend,
+        )
+
+    if method in {"gotoh-local", "gl"}:
+        return gotoh_local(
+            seq1,
+            seq2,
+            match_score=match_score,
+            mismatch_score=mismatch_score,
+            gap_open=gap_open,
+            gap_extend=gap_extend,
+        )
+
+    if method in {"gotoh-freeshift", "gf"}:
+        return gotoh_freeshift(
+            seq1,
+            seq2,
+            match_score=match_score,
+            mismatch_score=mismatch_score,
+            gap_open=gap_open,
+            gap_extend=gap_extend,
+        )
+
     raise ValueError(
-        "Unknown method. Supported methods are: 'nw', 'needleman-wunsch', 'sw', 'smith-waterman'."
+        "Unknown method. Use one of: nw, sw, gotoh-global, gotoh-local, gotoh-freeshift."
     )
